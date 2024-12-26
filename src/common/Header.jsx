59a779197dc6977864ecf8c36791858ca95a5eb9
AmitSheoran-557 <amit22082006@gmail.com>
2024-12-26 14:41:21 +0530

    first commit

---
+++ src/common/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <nav>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+       <button></button>  
+    </nav>
+  )
+}
+
+export default Header
